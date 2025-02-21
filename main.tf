d844d66df727788317a8c6692328c8b56c5601e1
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-02-21 15:29:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "adarsg5622"   # Your S3 bucket for storing Terraform state
+    bucket = "adarsh5622"   # Your S3 bucket for storing Terraform state
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
